8cfe2cf5b72a6d8764393e98c1375dbd1183d80f
dncrdz <carloventocilla11@gmail.com>
2026-05-09 15:32:29 +0800

    feat: add main window

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class Main{
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Fitness Tracker");
+        frame.setSize(800,600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
