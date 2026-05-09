14907fa0bfdecef63b8bc2466a9a1f52e5cac47f
dncrdz <carloventocilla11@gmail.com>
2026-05-09 15:40:46 +0800

    feat: add Heading and button

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 public class Main{
     public static void main(String[] args) {
         JFrame frame = new JFrame("Fitness Tracker");
         frame.setSize(800,600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout());
         frame.setVisible(true);
+
+        JLabel label = new JLabel("Welcome to Fitness Tracker");
+        frame.add(label);
+
+        JButton button = new JButton("Log Weight");
+        frame.add(button);
     }
 }
