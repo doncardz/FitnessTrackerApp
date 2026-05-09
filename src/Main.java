b445fb9773d59e3924ac77a79fb94a09ff723019
dncrdz <carloventocilla11@gmail.com>
2026-05-09 15:54:25 +0800

    feat: organized layout using BorderLayout

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,16 @@ public class Main{
         JFrame frame = new JFrame("Fitness Tracker");
         frame.setSize(800,600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new FlowLayout());
+        frame.setLayout(new BorderLayout());
         frame.setVisible(true);
 
         JLabel label = new JLabel("Welcome to Fitness Tracker");
-        frame.add(label);
+        frame.add(label, BorderLayout.NORTH);
 
         JButton button = new JButton("Log Weight");
-        frame.add(button);
+        frame.add(button, BorderLayout.SOUTH);
 
         JTextField weightInput = new JTextField(10);
-        frame.add(weightInput);
+        frame.add(weightInput, BorderLayout.CENTER);
     }
 }
